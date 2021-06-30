ee306b8646afe3f26a9cf4fa95b647f614d197c6
Andrés Zibula <30576177+andres-zibula@users.noreply.github.com>
2021-06-29 23:57:13 -0300

    added UART

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -43,6 +43,8 @@
 TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim3;
 
+UART_HandleTypeDef huart1;
+
 /* USER CODE BEGIN PV */
 TIM_HandleTypeDef *htim = &htim2;
 TIM_HandleTypeDef *htim_ms = &htim3;
@@ -54,6 +56,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_TIM3_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -70,6 +73,7 @@ static void MX_TIM3_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
+  uint8_t uart_buffer[80] = {0};
 
   /* USER CODE END 1 */
 
@@ -93,6 +97,7 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM2_Init();
   MX_TIM3_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
@@ -104,15 +109,27 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    lift();
+
+    HAL_UART_Receive (&huart1, uart_buffer, sizeof(uart_buffer), HAL_MAX_DELAY);
+
+    //procesar
+
+
+    HAL_UART_Transmit(&huart1, "OK\n", 3, HAL_MAX_DELAY);
+
+
+
+
+    //lift();
 
     HAL_Delay(1000);
 
-    go_to(0, 230);
+    //go_to(0, 230);
+    //put_down();
 
-    HAL_Delay(1000);
+    //HAL_Delay(1000);
 
-    go_to(0, 270);
+    //go_to(0, 270);
 
 
     /*draw_line(-75.0, 250.0, 175.0, 250.0, true);
@@ -288,6 +305,39 @@ static void MX_TIM3_Init(void)
   /* USER CODE BEGIN TIM3_Init 2 */
 
   /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
 
 }
 
